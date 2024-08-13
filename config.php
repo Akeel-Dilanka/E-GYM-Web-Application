f58c942731b0ccc6d454dad897b36da5cf3a6aba
Akeel Dilanka Fernando <125078547+Akeel-Dilanka@users.noreply.github.com>
2024-08-13 20:20:29 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
     require_once "stripe-php-master/init.php";
 
     $stripeDetails = array(
-        "secretKey" => "sk_test_51LiNF2EfuNBU2VIP1jTFADR5Kw9PMpXZtWDRrEndtvqFt6V2wXtKsBKFkcA3LQZ8aQCeu3i6eyMf8MGXc0Cwbbtr00uxqSMr9J" ,
-        "publishableKey" => "pk_test_51LiNF2EfuNBU2VIPr7oW9ZnyPNwpRgm3DoJOwg7ynXvUagfcMZzFXI6GGfaJvPlPBOFALBQhULqs8NuPwn23In8500Fb1RuIQ4"
+        "secretKey" => "key" ,
+        "publishableKey" => "key"
     );
 
     \Stripe\Stripe::setApiKey($stripeDetails["secretKey"]);
